540b3367612de78bb01b1a734e26789e34f0ff08
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-25 10:01:00 +0600

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Main from "./components/Main";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Main/>
     </div>
   );
 }
