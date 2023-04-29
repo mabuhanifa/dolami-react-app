54385374a9a7216cd70c61338bfe71aab6011add
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-29 19:58:00 +0600

    data mapped

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { data } from "../data/data";
 import SideBar from "./SideBar";
 
 export default function Main() {
@@ -6,10 +7,20 @@ export default function Main() {
     <section className="px-10">
       <div className="flex">
         <SideBar />
-        <div className="w-3/4 grid grid-cols-3">
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="w-3/4">
+          <div>
+            <h1>All Items</h1>
+          </div>
+          <div className=" grid grid-cols-4">
+            {data &&
+              data.map((data) => (
+                <div key={data.id}>
+                  <div className="p-1">
+                    <img src={data.img} alt="" className="rounded" />
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </section>
