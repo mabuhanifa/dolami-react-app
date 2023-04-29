67c04876d0d691895836a6d736ca5da7dfdbc7ed
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-29 19:47:39 +0600

    data created

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+import avatar from "../images/Avatar Pictures/VRC image A(F).png";
+
+export const data = [
+  {
+    name: "Avatar name “Avatown” -nice original avatar of Avatown",
+    img: avatar,
+    star: "★★★★★",
+    rating: 4.7,
+    likes: "1000",
+    love: false,
+    price: "$19.99",
+    pc_only: true,
+    desc: "Auto upload service ready, you can use this avatar within 24 hours.",
+  },
+];
