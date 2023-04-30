d8e15cb5dd732d4e42753bdc9458674f9a264ab5
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-01 00:50:57 +0600

    button styling

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { GrNext, GrPrevious } from "react-icons/gr";
 export default function Pagination({
   page,
   totalPages,
@@ -7,31 +7,36 @@ export default function Pagination({
   getNextPage,
   selectPageHandler,
 }) {
-  const isCurrent = (i) => {
-    if (i === page + 1) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
   return (
     <>
-      <div>
-        <button onClick={() => getPrevPage()}>PREV</button>
-        <p>
-          {page} of {totalPages}
-        </p>
-        {[...Array(totalPages)].map((_, i) => (
-          <span
-            onClick={() => selectPageHandler(i + 1)}
-            className={isCurrent(i) ? "" : ""}
-            key={i}
-          >
-            {i + 1}
-          </span>
-        ))}
-        <button onClick={() => getNextPage()}>NEXT</button>
+      <div className="mb-10">
+        <button
+          onClick={() => getPrevPage()}
+          className="text-xl font-bold text-gray-700"
+        >
+          <GrPrevious className="inline-block"/>
+        </button>
+        
+          {[...Array(totalPages)].map((_, i) => (
+            <button
+              onClick={() => selectPageHandler(i + 1)}
+              className={
+                i === page - 1
+                  ? "font-bold inline-block px-2 mx-1 bg-gray-300"
+                  : "text-gray inline-block px-2 mx-1"
+              }
+              key={i}
+            >
+              {i + 1}
+            </button>
+          ))}
+        
+        <button
+          onClick={() => getNextPage()}
+          className=" text-xl font-bold text-gray-700"
+        >
+          <GrNext className="inline-block"/>
+        </button>
       </div>
     </>
   );
