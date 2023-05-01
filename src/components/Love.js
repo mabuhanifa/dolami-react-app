0084ebb47ab9cb25ff86758a798a1a36e7dbca88
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-02 05:46:35 +0600

    share

--- src/components/Love.js
+++ src/components/Love.js
@@ -4,7 +4,7 @@ import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 export default function Love() {
   const [loved, setLoved] = useState(false);
   return (
-    <div onClick={() => setLoved(!loved)} className="text-xl">
+    <div onClick={() => setLoved(!loved)} className="text-xl cursor-pointer">
       {loved ? (
         <span>
           <VscHeartFilled className="text-red-500" />
