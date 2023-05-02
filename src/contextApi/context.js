370bf4007abacca2b55f387d071d457ad6fb0d94
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-02 22:48:26 +0600

    reducer

--- src/contextApi/context.js
+++ src/contextApi/context.js
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { data } from "../data/data";
 
 const Context = createContext();
 
+const initialState = {
+  state: data,
+  cart: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return { ...state, cart: [...state.cart, action.payload] };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
 const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return <Context.Provider value={{ rest: "hs" }}>{children}</Context.Provider>;
 };
 
+const useAvatar = () => {
+  return useContext(Context);
+};
 
 export default ContextProvider;
