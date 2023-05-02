8b828ca85b9cf7f26f689544e264844c9cfa5daa
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-02 06:52:08 +0600

    copied button

--- src/components/Share.js
+++ src/components/Share.js
@@ -7,15 +7,21 @@ export default function Share({ desc }) {
   return (
     <div className="relative">
       <span
-        className={`absolute right-2 top-[-20px] border border-gray-300 rounded px-1 pr-2 flex justify-end text-gray-500 cursor-pointer ${
+        className={`absolute right-2 top-[-20px] border border-gray-300 rounded px-1 pr-2 flex justify-end text-gray-500 cursor-pointer share ${
           share ? "" : "hidden"
         }`}
       >
+        <span
+          className="absolute right-2 bottom-[30px] font-[500] text-black bg-gray-400 text-xs px-1 rounded"
+          id="copy"
+        >
+          Copied
+        </span>
         <BsLink45Deg className="mt-1 mr-1" />
         Copy Link
       </span>
       <p>{desc}</p>
-      <div className="absolute right-3 bottom-1 text-gray-500">
+      <div className="absolute right-3 bottom-1 text-gray-500 ">
         <button onClick={() => setShare(!share)}>
           <FiShare className="text-lg" />
         </button>
